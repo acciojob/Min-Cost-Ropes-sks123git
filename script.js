7433336d0cea9a7c03f3f77866b13e2ebf489566
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-15 20:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+let cost = 0;
+
+    arr.sort((a, b) => a - b);
+
+    while (arr.length > 1) {
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let sum = first + second;
+        cost += sum;
+
+        arr.push(sum);
+        arr.sort((a, b) => a - b);
+    }
+
+    return cost;
   
 }
 
